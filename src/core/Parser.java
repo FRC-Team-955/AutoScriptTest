d8a589d78030aea32b2c893c1662a0cd47ff002f
Troy-Jameson <trevor.eric.j@gmail.com>
2016-01-17 18:16:45 -0800

    First iteration of parser, only returns same thing, doesn't parse

---
+++ src/core/Parser.java
@@ -0,0 +1,13 @@
+package core;
+
+import java.io.*;
+
+public class Parser{
+//	String fileName = ""; //TODO fix
+//	FileReader fileReader = new FileReader();	
+	public static double[][] parse() {
+		double i[][] = {{0,0,1},{1,10,0},{0,0,1}};
+		return i;
+	}
+	
+}
